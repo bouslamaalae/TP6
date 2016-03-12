04430232884f857791d84893dfb0e612716f6e4a
Alae <bouslama.alaeeddine@gmail.com>
2016-03-12 14:29:43 +0100

    test2

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tp6App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the tp6App
+ */
+angular.module('tp6App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
